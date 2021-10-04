971a16ed7d9a55d37ad20fe803b4c99075741f06
Dory Azar <dory.azar@gmail.com>
2021-10-04 18:34:18 -0400

    Adding classname to some components

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './Panel.css';
+import className from 'classnames';
 import PropTypes from 'prop-types';
 
 class Panel extends Component {
   render() {
     
     const { border, children }  = this.props;
+
+    const panelStyle = [{
+      "card": !!border
+    }]
     
-    return <div className={!!border ? "card" : '' }>{children}</div>;
+    return <div className={className(panelStyle)}>{children}</div>;
   }
 }
 
